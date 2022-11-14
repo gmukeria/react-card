9a2e8793ff00f79fc39388ddd45c59679ec52e6d
giorgi <giorgi.muqeria@gmail.com>
2022-11-14 21:39:22 +0400

    Updates

---
+++ precache-manifest.c2cd002b19cb8fe1d1a15899373ad857.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "7ada6c290ea5af6f6515",
+    "url": "/react-card/static/css/main.8b68e869.chunk.css"
+  },
+  {
+    "revision": "7ada6c290ea5af6f6515",
+    "url": "/react-card/static/js/main.7ada6c29.chunk.js"
+  },
+  {
+    "revision": "639d171b052b9a4ce351",
+    "url": "/react-card/static/js/1.639d171b.chunk.js"
+  },
+  {
+    "revision": "1b2da14b5ab6a2314adc",
+    "url": "/react-card/static/js/runtime~main.1b2da14b.js"
+  },
+  {
+    "revision": "6b4873c19d6e1ffb5a14eb38eeedf6e2",
+    "url": "/react-card/static/media/icon-ethereum.6b4873c1.svg"
+  },
+  {
+    "revision": "65201f364bbb63dc2ef7685a07864595",
+    "url": "/react-card/static/media/icon-clock.65201f36.svg"
+  },
+  {
+    "revision": "e39748ba9f90a36609c83cfb5849f413",
+    "url": "/react-card/static/media/image-equilibrium.e39748ba.jpg"
+  },
+  {
+    "revision": "ee17721f3acda5d56c6a4ed7b715c7f3",
+    "url": "/react-card/index.html"
+  }
+];
